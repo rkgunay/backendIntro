aa6515db2035062ce5a70e6cdc468104972b93ee
rkgunay <ridvankgunay@gmail.com>
2021-09-19 03:03:03 +0300

    Ders3Odev2 ve Odev1 notları

---
+++ Ders3Odev2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ders3Odev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Calculations calculations = new Calculations();
+            calculations.Add(a, b);
+            Console.WriteLine(a);
+
+            //Yukarıdaki kodlarda 12.satırdaki işlem konsola 50 yazdırır. Çünkü fonksiyonun içinde 
+            //a değerine 30 verdik. b değeri aynı kaldı(20). toplamı 50 oldu. 
+            //13.satırdaki değer de 10 değerini verdi. Fonksiyonun içinde 30 dediğimiz için değişmedi. 
+            // fonksiyonu çağırırken fonksiyon, a değişkeninin değerini ilk parametreye atadı. Ve sonra
+            // o parametreye 30 değerini atadı. a değişkenine dokunmadı. 
+
+            //eğer a değişkenin de değişmesini isteseydik fonksiyonu Add(ref int a, int b) olarak tanımlardık.
+            // Çağırıken de Add(ref int a, b) olarak çağırırdık. Böylece a değeri, fonksiyonun ilk parametresi
+            // ile bağlanmış oldu. Bu şekilde çalıştırsaydık 13. satırdaki değer 30 olurdu. 
+            // out da aynı fakat bunu kullanırken a = 10 dememize gerek yok 9.satırda. 
+            // direkt int a; olarak tanımlayabilirdik. bir değer olmazdı. 
+        }
+
+        public class Calculations
+        {
+            public void Add(int a, int b)
+            {
+                a = 30;
+                Console.WriteLine(a+b);
+            }
+        }
+    }
+}
--- ValueAndReferenceTypes/Program.cs
+++ ValueAndReferenceTypes/Program.cs
@@ -23,3 +23,40 @@ namespace ValueAndReferenceTypes
         }
     }
 }
+
+
+// ref keyword is used to use value type variables as a reference type variables. 
+// if i use add(ref number1, number2), number2 is in function variable. doesn't have an effect
+// outside of the function. But if i change number1 inside the function. It will change the value of 
+// number1 variable outside the function. 
+
+// and theres this fella called "out" Its like "ref" but in ref, you should declare a value to that 
+// variable. But "out" doesn't have that problem because it's not a picky bastard like "ref". 
+
+// there's this thing called default parameters. It's easy. Just adding a value to parameter while
+// declearing a function. Like add(int a, int b =10) But you should give right to left. Otherwise
+// IDE will be mad at us. It looks at first parameter and it thinks, so this parameter has a default
+// value. It expects other parameters to be with default value. And if all parameters has no value then
+// you cant give the first parameter a value. I explained very terrible. That's my power hiring people.
+// know your stuff. 
+
+// Overloading -- Very very easy. 
+// Make dozens of functions with same name but different number of parameters. Program will understand
+// which function you want to use, just by counting the numbers of parameters you entered. Easy peasy. 
+
+
+// Params -- Very practical
+// DO NOT MAKE dozens of functions with same name but different number of parameters. 
+// It wouldn't be DRY. Instead, use params. I want to write this because I don't want to forget
+// It must be last parameter because it contains unknow number of parameters. 
+
+/* 
+    static add(params int[] numbers) 
+    {
+        return numbers.sum();
+    }
+ 
+
+    This functions can take many parameters. 10, 100, 1... doesn't matter. Better then making 
+    100 functions. 
+ */
